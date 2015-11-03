6b147bae1e168acbd9a8c8775bb77320f378b18c
ivorpad <ivorjpc@gmail.com>
2015-11-03 16:11:09 -0430

    Added the Assets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'assets/index'
+
+  get 'assets/new'
+
+  get 'assets/create'
+
+  get 'assets/show'
+
+  get 'assets/edit'
+
+  get 'assets/update'
+
+  get 'assets/destroy'
+
   devise_for :users
   root to: 'welcome#index'
 
