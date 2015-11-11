48bb4d604082d293d799e64f2734e6a888e7727a
ivorpad <ivorjpc@gmail.com>
2015-11-10 23:10:26 -0430

    Assets can now be added via categories#show, added radio buttons to select a category when uploading an asset

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,16 +7,17 @@ class AssetsController < ApplicationController
 
   def new
     @asset = Asset.new
+    @project = Project.find(params[:project_id])
   end
 
   def create
     @project = Project.find(params[:project_id])
-    # @category = Category.find(params[:category_id])
     @asset = @project.assets.create(asset_params)
-
     @asset.user_id = current_user.id
-    # @asset.project_id = @project.id
-    @asset.category_id = params[:category][:category_id]
+
+    @category = Category.find(params[:asset][:category_ids])
+    @asset.category_id = params[:asset][:category_ids]
+    @category.assets << @asset
 
     if @asset.save
       flash[:notice] = "Created"
@@ -43,6 +44,6 @@ class AssetsController < ApplicationController
   private
 
   def asset_params
-    params.require(:asset).permit(:title)
+    params.require(:asset).permit(:title, :category_ids => [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,19 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'welcome#index'
-
+# resources :projects, :path => '' in order to get example.com/:project_id
   resources :projects do
     resources :category, controller: 'categories', as: :category do
-        resources :asset, controller: 'assets', as: :assets, shallow: true
+        resources :asset, controller: 'assets', as: :assets, only: [:show, :index]
       end
+  resources :asset, controller: 'assets', as: :assets, except: [:show, :index]
   end
 end
 
 # example.com/project/1/category/1
 # example.com/asset/1
 # example.com/project/1/asset/1/
-# BAD: example.com/project/1/category/1/asset/1/
+# BAD: example.com/my-project/my-category/my-asset/
 
 # OR example.com/my-project/images/cars/[:show]
 
