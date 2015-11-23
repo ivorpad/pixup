77214e2cbafc5e1d3bcad2ca6117dc70c271b599
ivorpad <ivorjpc@gmail.com>
2015-11-23 15:18:06 -0430

    Removed Refile

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,7 @@
 class Asset < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
-  attachment :asset_file
-
+  
   validates_presence_of :title, :category
 
   belongs_to :project
--- db/migrate/20151123022506_add_asset_file_to_assets.rb
+++
@@ -1,5 +0,0 @@
-class AddAssetFileToAssets < ActiveRecord::Migration
-  def change
-    add_column :assets, :asset_file_id, :string
-  end
-end
