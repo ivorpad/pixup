8b81e4e52e839ea10cd140025ea2e3ab73f8e24c
ivorpad <ivorjpc@gmail.com>
2016-01-20 12:24:37 -0430

    Fixed a bug where global categories where displaying empty assets created in other projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def all_categories(project)
+  def categories_for(project)
     global = Category.where(:global => true)
     no_global = project.categories.where(:global => false)
     global.concat(no_global)
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,6 @@
 module CategoriesHelper
+  # scope :assets_for, -> (project, category) { where( :project_id => project.id, :category_id => category.id  ) }
+  def assets_for(project, category)
+    Asset.where(:project_id => project.id, :category_id => category.id)
+  end
 end
