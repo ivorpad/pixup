69bf550df09e4e5bff6e17aa4b43af9ff7365573
ivorpad <ivorjpc@gmail.com>
2016-01-22 15:44:46 -0430

    Bugfix: Avoid Devise logout of admin once password is updated.
    Avoid Devise logout of admin once password is updated.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -21,6 +21,28 @@ ActiveAdmin.register User do
     end
     f.button "Save"
   end
-  
+
+  controller do
+  def update
+    # get the currently logged in User's id
+    current_id = current_admin_user.id
+    # load the User being updated
+    @user = User.find(params[:id])
+    # update the User with new attributes
+    # if successful, this will sign out the User being updated
+    if @user.update_attributes(permitted_params[:user])
+      # if the updated User was the currently logged in User, sign them back in
+      if @user.id == current_id
+        sign_in(User.find(current_id), :bypass => true)
+      end
+      flash[:notice] = I18n.t('devise.passwords.updated_not_active')
+      redirect_to '/admin/users'
+    # display errors
+    else
+      render :action => :edit
+    end
+  end
+end
+
   permit_params :user, :name, :password, :password_confirmation, :company, :email, :admin
 end
