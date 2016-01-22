bc7a1f8b0544dfea5bfd9f5b1df13cbb740bbf36
ivorpad <ivorjpc@gmail.com>
2016-01-22 15:39:07 -0430

    Removed unused block of code.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -21,22 +21,6 @@ ActiveAdmin.register User do
     end
     f.button "Save"
   end
-
-  # create_or_edit = Proc.new {
-  #   @user       = User.find_or_create_by(params[:id])
-  #   @user.admin = permitted_params[:user][:admin]
-  #   @user.attributes = permitted_params[:user].delete_if do |k, v|
-  #     (k == "admin") ||
-  #     (["password", "password_confirmation"].include?(k) && v.empty? && !@user.new_record?)
-  #   end
-  #   if @user.save
-  #     redirect_to :action => :show, :id => @user.id
-  #   else
-  #     render active_admin_template((@user.new_record? ? 'new' : 'edit') + '.html.erb')
-  #   end
-  # }
-  # member_action :create, :method => :post, &create_or_edit
-  # member_action :update, :method => :put, &create_or_edit
-
+  
   permit_params :user, :name, :password, :password_confirmation, :company, :email, :admin
 end
