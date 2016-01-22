8d0e6bd481abf68a53b54ccfeee9593006995282
ivorpad <ivorjpc@gmail.com>
2016-01-22 15:33:19 -0430

    Bugfix: Users can now be saved. Enhancement: Added a new a new resource for Projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  after_create { |admin| admin.send_reset_password_instructions }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :created_projects, class_name: "Project"
   has_many :assets, dependent: :destroy
 
-  after_create { |admin| admin.send_reset_password_instructions }
+  private
 
   def password_required?
     new_record? ? false : super
